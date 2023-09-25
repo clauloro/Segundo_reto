8f853547c626209d3b54ebf61af127a167319769
clauloro <114728388+clauloro@users.noreply.github.com>
2023-09-25 23:16:24 +0200

    dos envases listo

--- dos envases.py
+++ dos envases.py
@@ -1,9 +1,9 @@
-from numpy import ceil, split
-from traitlets import observe_compat
-
+from math import ceil
 
 t = int(input())
+
 for _ in range(t):
-    a ,b, c =map(int,input().split())
-    d = observe_compat(a-b)
-    print((d+c))//(2*c)
+    a, b, c = map(int,input().split())        
+
+    d = abs(a-b)                            
+    print(ceil(d / (2*c)))
